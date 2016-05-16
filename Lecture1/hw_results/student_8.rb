91cc79aa2703aba899bead4e4aa1e032b7aa4e6f
Denis Grebenyuk <d.grebenyuk@svitla.com>
2016-05-16 20:31:44 +0300

    fix Anton's homework

--- Lecture1/hw_results/student_8.rb
+++ Lecture1/hw_results/student_8.rb
@@ -1,2 +1,2 @@
 my_hash = {"yes"=>23, "b"=>"travel", "yesterday"=>34, 5=>"234", :yesss=>:fg, try: 30, key: "some value", "yesterday"=>34, "yesteryear"=>2014}.keys
-my_hash.find_all{|key| key =~ /yes/}.size
+my_hash.find_all{|key| key =~ /^yes/}.size
