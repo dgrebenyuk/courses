719d8676b36a34d4bc0cb7e5afe143480fb52352
Denis Grebenyuk <d.grebenyuk@svitla.com>
2016-06-08 17:50:10 +0300

    add groups table

--- Rails/db/schema.rb
+++ Rails/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160606173650) do
+ActiveRecord::Schema.define(:version => 20160608133114) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -20,6 +26,13 @@ ActiveRecord::Schema.define(:version => 20160606173650) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+  end
+
+  create_table "user_groups", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
