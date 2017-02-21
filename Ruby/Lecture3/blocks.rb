856cd0b20e20c37dea318dde9fca0d218c5377a4
Denis Grebenyuk <d.grebenyuk@svitla.com>
2017-02-21 12:48:18 +0200

    making this thing actually usable.

--- Ruby/Lecture3/blocks.rb
+++ Ruby/Lecture3/blocks.rb
@@ -1,3 +1,20 @@
+def block_method1(&block)
+  yield # Error if no block
+end
+
+def block_method2(&block)
+  yield if block_given?
+end
+
+def block_method3(&block)
+  name = "SoftGroup"
+  yield(name) if block_given?
+end
+
+def simple_method
+  p name
+end
+
 def execute_lam
   lam = lambda {|a| return a+1}
   x = lam.call(1)
